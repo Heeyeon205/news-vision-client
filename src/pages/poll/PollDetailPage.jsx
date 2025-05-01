921485f563a262ec5ad6959585bc168190f1c4d6
Songmyungbo <audqh656@gmail.com>
2025-05-01 12:17:03 +0900

    feat:0501 투표 디테일 페이지 작업중 중간마지

--- src/pages/poll/PollDetailPage.jsx
+++ src/pages/poll/PollDetailPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { useStore } from "../../store/useUserStore";
+import "./PollDetail.css";
 
 export default function PollDetailPage() {
   const logId = useStore((state) => state.userId);
@@ -52,8 +53,10 @@ export default function PollDetailPage() {
     }
   };
 
+  const totalVotes = options.reduce((sum, option) => sum + option.count, 0); //명보가 추가가
+
   return (
-    <div className="pollContainer">
+    <div className="p-4 max-w-[600px] w-full mx-auto">
       <div>
         <p>{expiredAt}</p>
         <p>{title}</p>
@@ -64,17 +67,24 @@ export default function PollDetailPage() {
       <hr />
 
       {options.map((option) => (
-        <div className="optionBox">
+        <div className="optionBox mb-5">
           <div key={option.id}>
             {isVote ? (
-              <p>{option.content}</p>
+              <div>
+                <p>{option.content}</p>
+                <progress max={totalVotes} value={option.count} className="w-full voted-progress"></progress>
+              </div> // 투표 완료 했을때 보여주는 막대
             ) : (
               <>
                 <p>
-                  {option.content} : {option.count}
+                  {option.content}
                 </p>
+                <div>
+                  <progress max={totalVotes} value={option.count} className="w-full default-progress"></progress>
+                  <span>{option.count}</span>
+                </div>
                 <button
-                  className="bg-orange-500 hover:bg-orange-600 text-white text-sm px-4 py-2 rounded"
+                  className="bg-orange-500 hover:bg-orange-600 text-white text-sm px-4 py-2 rounded mt-2"
                   onClick={() => handleClick(option.id)}
                 >
                   투표하기
