07d72ff81486199aaf5be74ab6128d1c753a13af
jiwon0324 <wonjj0324@gmail.com>
2025-05-04 16:55:32 +0900

    feat: 빈 벨아이콘 방법 두가지 NoticeButton.jsx 19 ~47 줄에 있습니다

--- src/pages/notice/NoticeButton.jsx
+++ src/pages/notice/NoticeButton.jsx
@@ -16,7 +16,7 @@ import Notice from './Notice';
 //     }
 //   };
 
-// 비어있는 아이콘 쓰는 법
+// 비어있는 아이콘 쓰는 법 방법 1
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faBell } from '@fortawesome/free-regular-svg-icons';
 
@@ -28,6 +28,23 @@ import Notice from './Notice';
 //     </div>
 //   );
 // }
+
+// 비어있는 아이콘 쓰는 법 방법 1
+// <svg
+//xmlns="http://www.w3.org/2000/svg"
+//width="20"
+//height="20"
+//viewBox="0 0 24 24"
+//fill="none"
+//stroke="currentColor"
+//stroke-width="2"
+//stroke-linecap="round"
+//stroke-linejoin="round"
+//class="feather feather-bell"
+//>
+//<path d="M18 8a6 6 0 0 0-12 0c0 7-3 9-3 9h18s-3-2-3-9"></path>
+//<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+//</svg>
 
 export default function NoticeDropdown() {
   const [isOpen, setIsOpen] = useState(false);
